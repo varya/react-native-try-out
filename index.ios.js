d100fe687d88781a5d86467cb429d04419d3f5ea
Varya Stepanova <mail@varya.me>
2015-08-18 00:38:36 +0300

    Button and alert

--- index.ios.js
+++ index.ios.js
@@ -5,6 +5,10 @@
 'use strict';
 
 var React = require('react-native');
+var Redux = require('redux');
+
+var Button = require('react-native-button');
+
 var {
   AppRegistry,
   StyleSheet,
@@ -12,16 +16,31 @@ var {
   View,
 } = React;
 
+var Counter = React.createClass({
+  render: function() {
+    return (
+      <View style={styles.counter}>
+        <Text>I am here</Text>
+        <Button onPress={this._handlePress}>Press me!</Button>
+      </View>
+    )
+  },
+  _handlePress(event) {
+    alert('Pressed!');
+  }
+})
+
 var reactNativeTryOut = React.createClass({
   render: function() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native! XXX
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
         </Text>
+        <Counter/>
         <Text style={styles.instructions}>
           Press Cmd+R to reload,{'\n'}
           Cmd+D or shake for dev menu
@@ -48,6 +67,9 @@ var styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  counter: {
+    backgroundColor: 'pink',
+  }
 });
 
 AppRegistry.registerComponent('reactNativeTryOut', () => reactNativeTryOut);
